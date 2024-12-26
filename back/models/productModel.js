6e6bf57247a4d1b10c5f6dfaf29b48b048a72866
WahabBello <abdouwahab123@gmail.com>
2024-12-26 14:24:17 +0100

    Modélisation des données :
    - Concevoir les modèles MongoDB pour les utilisateurs, produits, ventes, et rapports.

--- back/models/productModel.js
+++ back/models/productModel.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const productSchema = new mongoose.Schema({
   name: { type: String, required: true },
   price: { type: Number, required: true },
+  stock: { type: Number, required: true }, // Quantité disponible
+  category: { type: String }, // Catégorie pour le regroupement
   createdAt: { type: Date, default: Date.now }
 });
 
--- back/models/salesModel.js
+++ back/models/salesModel.js
@@ -1,9 +1,15 @@
 const mongoose = require('mongoose');
 
 const saleSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  price: { type: Number, required: true },
-  createdAt: { type: Date, default: Date.now }
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, // Référence à un utilisateur
+  products: [
+    {
+      product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true }, // Référence à un produit
+      quantity: { type: Number, required: true },
+    },
+  ],
+  totalAmount: { type: Number, required: true }, // Montant total de la vente
+  saleDate: { type: Date, default: Date.now }, // Date de la vente
 });
 
 module.exports = mongoose.model('Sale', saleSchema);
